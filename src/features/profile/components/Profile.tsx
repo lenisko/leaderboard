514a83bdf10b11038e6ee0534da0b43fa0856c5d
lenisko <10072920+lenisko@users.noreply.github.com>
2025-06-08 23:46:07 +0200

    feat: center profile section view

--- src/features/profile/components/Profile.tsx
+++ src/features/profile/components/Profile.tsx
@@ -12,34 +12,28 @@ interface ProfileProps {
 
 export const Profile = ({ trainer }: ProfileProps): JSX.Element => {
   const isDesktop = useBreakpoint('lg');
-  const isMobile = !isDesktop;
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
   useEffect(() => {
     setSelectedTabIndex(0);
-  }, [isDesktop, setSelectedTabIndex]);
+  }, [isDesktop]);
 
   return (
-    <>
+    <div className="lg:w-[60%] lg:mx-auto">
       <div className="lg:grid lg:grid-cols-12 lg:gap-5">
-        <div className="lg:col-span-7">
+        <div className="lg:col-span-12">
           <TrainerCard trainer={trainer} />
           <div className="mt-7">
             <Group selectedIndex={selectedTabIndex} onChange={setSelectedTabIndex}>
               <Panels className="mt-5">
-                {isMobile && (
-                  <Panel>
-                    <OverviewCards trainer={trainer} />
-                  </Panel>
-                )}
+                <Panel>
+                  <OverviewCards trainer={trainer} />
+                </Panel>
               </Panels>
             </Group>
           </div>
         </div>
-        <div className="hidden lg:grid lg:col-span-5">
-          <OverviewCards trainer={trainer} />
-        </div>
       </div>
-    </>
+    </div>
   );
 };
